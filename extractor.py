bbca5082b8da7d5d24b313c5c52c59ca913f9aa2
smmerrit <smerritt98@gmail.com>
2020-06-19 11:31:36 -0700

    first commit

--- extractor.py
+++ extractor.py
@@ -1,1 +1,35 @@
-p
+#! python3
+import pyperclip, re
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))? # area code
+    (\s|-|\.)?         # separator
+    (\d{3})              # first 3 digits
+    (\s|-|\.)          # separator
+    (\d{4})              # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
+    )''', re.VERBOSE)
+
+email = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+      # username
+    @                      # @ symbol
+    [a-zA-Z0-9.-]+         # domain name
+    (\.[a-zA-Z]{2,4}){1,2} # dot-something
+    )''', re.VERBOSE)
+
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum ='-'.join([groups[1],groups[3],groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+        matches.append(phoneNum)
+for groups in email.findall(text):
+    matches.append(groups[0])
+
+if len(matches)>0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboiard')
+    print('\n'.join(matches))
+else:
+    print('No info found!')
